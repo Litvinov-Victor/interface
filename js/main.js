8b81d5615166cc2c923db0c71aa62cbdf462c83d
Vitaliy Ryazanov <vitaliy0332@gmial.com>
2017-05-18 18:15:33 +0500

    Скрипт ресайза правой колонки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 $(function(){
 
-	$(document).on('click','ul.tabs li:not(.active)', function(e) {
-    	e.preventDefault();
-		$(this).addClass('active').siblings().removeClass('active');
-		$('.tabs__contents').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    $(document).on('click','ul.tabs li:not(.active)', function(e) {
+        e.preventDefault();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.tabs__contents').find('.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
+
+    ;(function(){
+        var resize    = $(".interface__right-pan");
+        var pannel    = $(".interface__right");
+        var currWidth = pannel.width();
+        var unlock    = false;   
+        $(document).mousemove(function(e) {
+            var change = $(window).width() - e.clientX;
+           if(unlock) {
+                pannel.css("width", change);
+            }
+        });     
+        resize.mousedown(function(e) {
+            currWidth = pannel.width();
+            unlock     = true;
+        });
+        $(document).mousedown(function(e) {
+            if(unlock) {
+              e.preventDefault();
+            }
+        });
+        $(document).mouseup(function(e) {
+            unlock = false;
+        });
+    })();
     
 })
